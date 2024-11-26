dcc30aaa3c6dbb52f3d7dde5bb9ebb99b79ead84
Harshith Varma Endla <135434865+Harshith-endla@users.noreply.github.com>
2024-11-26 21:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -112,6 +112,6 @@ app.post('/calculator', (req, res) => {
 });
 
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
